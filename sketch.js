a85460f486d0603f0350fde29b35e04609567165
takumi shinzato <takumusic0873@gmail.com>
2025-09-18 13:38:00 +0900

    メディアアート作成

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let ballNum;
+let x;
+let y;
+let vx;
+
 function setup() {
     createCanvas(400, 400);
+    ballNum = 25;
+    x = [];
+    for (let i = 0; i < ballNum; i++) {
+        x.push((width / (ballNum + 1)) * (i + 1));
+    }
+    y = 0;
+    vx = 8;
 }
 
 function draw() {
-    background(220);
+    background(0, 0, 0, 20);
+
+    y = y + vx;
+    if (y > height) {
+        y = 0;
+    }
+
+    fill(random(128, 255), random(128, 255), 255);
+    for (let i = 0; i < x.length; i++) {
+        circle(x[i], y, random(0, 10));
+    }
 }
